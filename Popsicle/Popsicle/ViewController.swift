828660bbc40dc3bea86b64f8cca6b5464a870c30
Ilter Canberk <iltercanberk@gmail.com>
2014-09-26 12:04:07 -0400

    initial commit

---
+++ Popsicle/Popsicle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Popsicle
+//
+//  Created by Ilter Canberk on 9/26/14.
+//  Copyright (c) 2014 hax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
