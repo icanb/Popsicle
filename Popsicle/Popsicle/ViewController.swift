ee0e8c24fa84ebf591fff66c06b5c54eb1e84b9d
Mirai Akagawa <mirai418@me.com>
2014-09-27 15:58:17 -0400

    temp stuff

--- Popsicle/Popsicle/ViewController.swift
+++ Popsicle/Popsicle/ViewController.swift
@@ -32,14 +32,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        var lol = TFHpple
-        
+
         let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"
         
-        var err : NSError?
+    
         
-        var parser     = TFHpple(html: html, error: &err)
+        var err : NSError?
+        var parser     = HTMLParser(html: html, error: &err)
         if err != nil {
             println(err)
             exit(1)
@@ -60,6 +59,7 @@ class ViewController: UIViewController {
             }
         }
 
+        
         
         println("cheeeel")
         let url = NSURL(string: "http://google.com")
