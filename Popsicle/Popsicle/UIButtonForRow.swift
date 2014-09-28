398f8e76b734993832f15e1034d260ddca017ff0
Ilter Canberk <iltercanberk@gmail.com>
2014-09-28 14:58:24 -0400

    sub table

---
+++ Popsicle/Popsicle/UIButtonForRow.swift
@@ -0,0 +1,16 @@
+//
+//  UIButtonForRow.swift
+//  Popsicle
+//
+//  Created by Ilter Canberk on 9/28/14.
+//  Copyright (c) 2014 hax. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UIButtonForRow:UIButton {
+
+    var indexPath:NSIndexPath?
+
+}
